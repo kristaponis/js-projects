b90f087e505c94797da5a924198e3cfa79630426
kristaponis <a.kristaponis@icloud.com>
2020-12-10 13:20:55 +0200

    Added javascript to the rest of the elements

--- hangman/main.js
+++ hangman/main.js
@@ -19,10 +19,64 @@ function showWord() {
     word.innerHTML = `${selectedWord.split("").map(letter => ` <span class="letter">${correctGuess.includes(letter) ? letter : ""}</span>`).join("")}`
     const innerWord = word.innerText.replace(/\n/g, "")
     if (innerWord === selectedWord) {
-        message.innerText = "You won!"
+        message.innerText = "You won :)"
         popupContainer.style.display = "flex"
 
     }
 }
+
+function showInfo() {
+    infoContainer.classList.add("show")
+    setTimeout(() => {
+        infoContainer.classList.remove("show")
+    }, 2000)
+}
+
+function updateWrongGuess() {
+    wrongLetters.innerHTML = `
+    ${wrongGuess.length > 0 ? "<p>Wrong</p>" : ""} 
+    ${wrongGuess.map(letter => `<span>${letter}</span>`)}`
+    figureParts.forEach((part, index) => {
+        const err = wrongGuess.length
+        if (index < err) {
+            part.style.display = "block"
+        } else {
+            part.style.display = "none"
+        }
+    })
+    if (wrongGuess.length === figureParts.length) {
+        message.innerText = "You lost :("
+        popupContainer.style.display = "flex"
+    }
+}
+
+window.addEventListener("keydown", e => {
+    const letter = e.key
+    console.log(letter)
+    if (selectedWord.includes(letter)) {
+        if (!correctGuess.includes(letter)) {
+            correctGuess.push(letter)
+            showWord()
+        } else {
+            showInfo()
+        }
+    } else {
+        if (!wrongGuess.includes(letter)) {
+            wrongGuess.push(letter)
+            updateWrongGuess()
+        } else {
+            showInfo()
+        }
+    }
+})
+
+play.addEventListener("click", () => {
+    correctGuess.splice(0)
+    wrongGuess.splice(0)
+    selectedWord = someWords[Math.floor(Math.random() * someWords.length)]
+    showWord()
+    updateWrongGuess()
+    popupContainer.style.display = "none"
+})
 
 showWord()
